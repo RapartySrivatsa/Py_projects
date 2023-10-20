f3bf51fdbb88a60970803119af41bc2581d9bb7f
Raparty Sri Vatsa <srivatsasairam14@gmail.com>
2023-10-20 13:34:19 +0200

    simple math lib programs

---
+++ simple_math/circum/circum.py
@@ -0,0 +1,30 @@
+# Circum of a circle
+""""
+import math
+
+radius = float(input("Enter the value of the radius: "))
+
+circum = 2 * math.pi * radius
+
+print(f"The circumference is: {round(circum,2)}cm")
+"""
+# Area of a Circle
+"""
+import math
+
+radius = float(input("Enter the value of the radius: "))
+
+area = math.pi * pow(radius, 2)
+
+print(f"The Area of circle is: {round(area,2)}cm^2")
+"""
+
+# Hypotenuse of a right triangle
+import math
+
+a = float(input("Enter the value of a side A: "))
+b = float(input("Enter the value of a side B: "))
+
+c = math.sqrt(math.pow(a, 2) + math.pow(b, 2))
+
+print(f"The hypotenuse of a triangle is: {round(c,2)}cm")
